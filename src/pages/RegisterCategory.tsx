c4b85ce3dcc84a96f6089952019806179151331a
João Marcelo <joaomarcelocpa0303@gmail.com>
2025-10-15 12:54:18 -0300

    fixes

--- src/pages/RegisterCategory.tsx
+++ src/pages/RegisterCategory.tsx
@@ -1,4 +1,3 @@
-// import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import RegisterCategoryForm from '../components/RegisterCategoryForm';
